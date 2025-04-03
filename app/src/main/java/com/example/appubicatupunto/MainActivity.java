114f9ea573619370a559286d478ad8015a04cbab
Daglos <102191213+Daglos@users.noreply.github.com>
2025-04-03 01:42:21 -0600

    Mejoras del MainAtivity: -Carga de nuevo logo (img) -Nuevo logo en inicio sesion -Validacion campos vacios (email, contraseña)

--- app/src/main/java/com/example/appubicatupunto/MainActivity.java
+++ app/src/main/java/com/example/appubicatupunto/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.appubicatupunto;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +13,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText campocorreo, campocontrasena;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +26,33 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        campocorreo = findViewById(R.id.editTextTextEmailAddress);
+        campocontrasena = findViewById(R.id.editTextTextPassword);
     }
 
+    //METODO DEL BOTON CAMBIAR CONTRASEÑA
+    public void Contra(View view){
+        Intent contra = new Intent(this,ChangePassword.class);
+        startActivity(contra);
+    } //FIN METODO DEL BOTON CAMBIAR CONTRASEÑA
+
+    //METODO DEL ENLACE PARA CREAR USUARIO
+    public void crearusuario(View view){
+        Intent crearusuario = new Intent(this,CreateUser.class);
+        startActivity(crearusuario);
+    } //FIN METODO PARA CREAR USUARIO
+
+    //METODO PARA INICIAR SESION
+    public void login(View view) {
+        String correo = campocorreo.getText().toString().trim();
+        String contrasena = campocontrasena.getText().toString().trim();
+
+        if (correo.isEmpty() || contrasena.isEmpty()) {
+            Toast.makeText(this, "Por favor, ingresa tu correo y contraseña", Toast.LENGTH_SHORT).show();
+        }else {
+            Intent login = new Intent(this,Selection.class);
+            startActivity(login);
+        }
+     //FIN METODO PARA INICIAR SESION
+    }
 }
