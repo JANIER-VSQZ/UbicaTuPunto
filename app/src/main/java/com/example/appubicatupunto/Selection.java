186a556df9a75af19aaabb91fd232443e3260b5c
JANIER-VSQZ <vasquezjanier31@gmail.com>
2025-04-05 11:09:57 -0600

    Ocultar scrollView si no hay nada seleccionado

--- app/src/main/java/com/example/appubicatupunto/Selection.java
+++ app/src/main/java/com/example/appubicatupunto/Selection.java
@@ -3,8 +3,13 @@ package com.example.appubicatupunto;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ScrollView;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,8 +17,12 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+
+
 public class Selection extends AppCompatActivity {
 int selectedOption = 0;
+private Spinner cbCiudad;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -31,16 +40,27 @@ int selectedOption = 0;
         ImageView imgBus = findViewById(R.id.btnBus);
         Button btnDetalles = findViewById(R.id.btnDetalles);
         Button btnImagen= findViewById(R.id.btnImagen);
+        Button btnDetalles2 = findViewById(R.id.btnDetalles2);
+        Button btnImagen2= findViewById(R.id.btnImagen2);
+        cbCiudad = (Spinner) findViewById(R.id.cbCiudad);
+        ScrollView scvPrincipal = findViewById(R.id.scvContenido);
+        TextView txtAdvertencia = findViewById(R.id.txtAdvertencia);
+
+        agregarCB();
 
         imgBus.setOnClickListener(v -> {
             selectedOption = 1;
             imgBus.setAlpha(1.0f);
             imgTaxi.setAlpha(0.5f); // Reduce la opacidad del otro
+            scvPrincipal.setVisibility(View.VISIBLE);
+            txtAdvertencia.setVisibility(View.INVISIBLE);
         });
         imgTaxi.setOnClickListener(v -> {
             selectedOption = 2;
             imgTaxi.setAlpha(1.0f);
             imgBus.setAlpha(0.5f); // Reduce la opacidad del otro
+            scvPrincipal.setVisibility(View.VISIBLE);
+            txtAdvertencia.setVisibility(View.INVISIBLE);
         });
 
         btnImagen.setOnClickListener(v->{
@@ -62,6 +82,41 @@ int selectedOption = 0;
                 startActivity(Ttaxi);
             }
         });
+
+        btnImagen2.setOnClickListener(v->{
+            if (selectedOption==1){
+                Intent TBus = new Intent(this, GaleryViewBus.class);
+                startActivity(TBus);
+            } else if (selectedOption==2) {
+                Intent Ttaxi = new Intent(this, GaleryViewTaxis.class);
+                startActivity(Ttaxi);
+            }
+        });
+
+        btnDetalles2.setOnClickListener(v->{
+            if (selectedOption==1){
+                Intent TBus = new Intent(this, MapViewBus.class);
+                startActivity(TBus);
+            } else if (selectedOption==2) {
+                Intent Ttaxi = new Intent(this, MapViewTaxis.class);
+                startActivity(Ttaxi);
+            }
+        });
+    }
+
+
+
+
+
+    public void agregarCB(){
+        ArrayList<String> datosCB = new ArrayList<>();
+        datosCB.add("Tegucigalpa");
+        datosCB.add("otro");
+        ArrayAdapter<String> adaptador = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, datosCB);
+        adaptador.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // Vincular adaptador al spinner
+        cbCiudad.setAdapter(adaptador);
     }
 
 
